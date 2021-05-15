23a183d93c097d9d4d26852114c701383b4ff1c5
Edmund Rotimi <support@scholarometer.com>
2021-05-15 10:32:58 +0100

    update mailer views.py file

--- app/mailer/views.py
+++ app/mailer/views.py
@@ -1,12 +1,20 @@
-#-*- coding: utf-8 -*-
-from django.shortcuts import render
-# Create your views here.
 from django.views.generic import ListView
-
-from mailer.models import Company
+from .models import Company, Contact, Order
 
 
 class IndexView(ListView):
-    template_name = "mailer/index.html"
     model = Company
+    template_name = "mailer/index.html"
+    context_object_name= 'companies'
     paginate_by = 100
+
+    def get_queryset(self):
+        return Company.companyquery.group_by('name', 'bic', 'number_of_orders',
+                                            'total_orders', 'total_paid', 'order_receipts')
+    
+    
+
+
+
+
+ 
